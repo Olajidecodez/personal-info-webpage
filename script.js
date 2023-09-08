490a7b6ed5119b4cde87e58b0c2c824dcc64bdbf
Balogun-olajide <olajidebalogun@icloud.com>
2023-09-08 17:39:35 +0100

    added style + changes in script and index

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const slackUserName = document.getElementById('slackUserName');
-  slackUserName.textContent = 'Olajide_';
-
   const slackDisplayImage = document.getElementById('slackDisplayImage');
   slackDisplayImage.src = 'Olajide.jpg';
   slackDisplayImage.alt = 'Olajide_';
+
+  const slackUserName = document.getElementById('slackUserName');
+  slackUserName.textContent = 'Olajide_';
 
   const currentDayOfTheWeek = document.getElementById('currentDayOfTheWeek');
   const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const currentDate = new Date();
   const dayIndex = currentDate.getDay();
   currentDayOfTheWeek.textContent = daysOfWeek[dayIndex];
+  
+  const currentUTCTime = document.getElementById('currentUTCTime');
+  const utcMilliseconds = Date.now();
+  currentUTCTime.textContent = utcMilliseconds;
+  
+
+  const myTrack = document.getElementById('myTrack');
+  myTrack.textContent = 'Frontend';
+
+  const githubURL = document.getElementById('githubURL');
+  githubURL.href = 'https://github.com/Olajidecodez/personal-info-webpage';
+
 })
